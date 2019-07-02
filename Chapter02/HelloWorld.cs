dbe42176d15015e15cae70ac58b3efd699b370b7
NaHyKim <koloke1712@gmail.com>
2019-07-02 20:28:28 +0900

    Create HelloWorld.cs

---
+++ Chapter02/HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStudy
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello, World!\n");
+            Console.Write("테스트\n");
+        }
+    }
+}
