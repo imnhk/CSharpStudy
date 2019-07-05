55769eaa09b2ea189d78065f8c6f278dbbf32b6d
NaHyKim <koloke1712@gmail.com>
2019-07-05 21:45:46 +0900

    Add Ch6, 7

---
+++ Chapter06/Method.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStudy
+{
+    class Method
+    {
+        static void Divide(int a, int b, out int quotient, out int remainder)
+        {
+            quotient = a / b;
+            remainder = a % b;
+        }
+
+        static int Plus(int a, int b)
+        {
+            return a + b;
+        }
+
+        static double Plus(double a  = 0, double b = 0)
+        {
+            return a + b;
+        }
+
+        static int Plus(params int[] args)
+        {
+            int sum = 0;
+            foreach (int num in args)
+                sum += num;
+            return sum;
+        }
+
+        static void PrintProfile(string name, string phone)
+        {
+            Console.WriteLine($"Name: {name}, Phone: {phone}");
+        }
+
+        // EX6_1
+        static double Square(double arg)
+        {
+            return arg * arg;
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 20;
+            int b = 3;
+            int c;
+            int d;
+
+            Divide(a, b, out c, out d);
+
+            Console.WriteLine($"{a}, {b}, {c}, {d}");
+            Console.WriteLine(Plus(a,b,c,d));
+            PrintProfile(phone: "000-00002-22", name: "nameme");
+
+            Console.WriteLine(Square(3));
+            Console.WriteLine(Square(34.2));
+
+        }
+    }
+}
---
+++ Chapter07/Class.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStudy
+{
+    class MyClass
+    {
+        public int myValue;
+
+        public MyClass()
+        {
+            myValue = 0;
+        }
+
+        public MyClass(int val)
+        {
+            myValue = val;
+        }
+
+        public MyClass DeepCopy()
+        {
+            MyClass copy = new MyClass();
+            copy.myValue = myValue;
+            return copy;
+        }
+    }
+
+    class MyDerivedClass : MyClass
+    {
+        public int newVal;
+        public MyDerivedClass(int val) : base(val)
+        {
+            newVal = 100;
+        }
+    }
+
+    public class MyClonableClass : ICloneable
+    {
+        public int myValue;
+        public Object Clone()
+        {
+            MyClonableClass copy = new MyClonableClass();
+            copy.myValue = myValue;
+            return copy;
+        }
+    }
+
+    class ArmorSuite
+    {
+        public virtual void Initialize()
+        {
+            Console.WriteLine("Armored");
+        }
+    }
+
+    class Ironman : ArmorSuite
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+            Console.WriteLine("Repulsor Rays Armed");
+        }
+    }
+
+    class WarMachine : ArmorSuite
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+            Console.WriteLine("Cannons Armed");
+        }
+    }
+
+    public static class IntegerExtension
+    {
+        public static int Square(this int myint)
+        {
+            return myint * myint;
+        }
+
+        public static int Power(this int myInt, int exponent)
+        {
+            int result = myInt;
+            for (var i = 1; i < exponent; i++)
+                result *= myInt;
+            return result;
+        }
+    }
+
+    class Class
+    {
+        static void Main(string[] arge)
+        {
+            ArmorSuite armorSuite = new ArmorSuite();
+            ArmorSuite ironMan = new Ironman();
+            ArmorSuite warMachine = new WarMachine();
+
+            armorSuite.Initialize();
+            ironMan.Initialize();
+            warMachine.Initialize();
+
+            Console.WriteLine(3.Square());
+            Console.WriteLine(4.Power(4));
+
+        }
+    }
+}
+
+
