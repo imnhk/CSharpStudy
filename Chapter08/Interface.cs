fe7f137b6741961e2ce85e53dd5e71d4dcbbb0ee
NaHyKim <koloke1712@gmail.com>
2019-07-07 11:24:00 +0900

    Add ch08

---
+++ Chapter08/Interface.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CSharpStudy
+{
+    interface ILogger
+    {
+        void WriteLog(string message);
+    }
+
+    class ConsoleLogger : ILogger
+    {
+        public void WriteLog(string message)
+        {
+            Console.WriteLine("{0} {1}", DateTime.Now.ToLocalTime(), message);
+        }
+    }
+
+    class FileLogger : ILogger
+    {
+        private StreamWriter writer;
+
+        public FileLogger(string path)
+        {
+            writer = File.CreateText(path);
+            writer.AutoFlush = true;
+        }
+
+        public void WriteLog(string message)
+        {
+            writer.WriteLine("{0} {1}", DateTime.Now.ToShortTimeString(), message);
+        }
+    }
+
+    interface IFormattableLogger : ILogger
+    {
+        void WriteLog(string format, params Object[] args);
+    }
+
+    class ConsoleLogger2 : IFormattableLogger
+    {
+        public void WriteLog(string message)
+        {
+            Console.WriteLine("{0} {1}", DateTime.Now.ToLocalTime(), message);
+        }
+
+        public void WriteLog(string format, params Object[] args)
+        {
+            String message = String.Format(format, args);
+            Console.WriteLine("{0} {1}", DateTime.Now.ToLocalTime(), message);
+        }
+    }
+
+    class ClimateMonitor
+    {
+        private ILogger logger;
+        public ClimateMonitor(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        public void Start()
+        {
+            while (true)
+            {
+                Console.Write("온도를 입력해주세요: ");
+                string temperature = Console.ReadLine();
+                if (temperature == "")
+                    break;
+                logger.WriteLog("현재 온도: " + temperature);
+            }
+        }
+    }
+
+    interface IRunnable
+    {
+        void Run();
+    }
+
+    interface IFlyable
+    {
+        void Fly();
+    }
+
+    class FlyingCar : IRunnable, IFlyable
+    {
+        public void Run()
+        {
+            Console.WriteLine("Runnnnnn");
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine("Flyyyyy");
+        }
+    }
+
+    abstract class AbstractClass
+    {
+        protected void PrivateMethodA()
+        {
+            Console.WriteLine("absA");
+        }
+        
+    }
+
+    class Interface
+    {
+        static void Main(string[] args)
+        {
+            IFormattableLogger logger = new ConsoleLogger2();
+
+            logger.WriteLog("{0} + {1} = {2}", 1, 1, 2);
+
+            FlyingCar car = new FlyingCar();
+            car.Run();
+            car.Fly();
+
+            IRunnable runnable = car as IRunnable;
+            IFlyable flyable = car as IFlyable;
+            runnable.Run();
+            flyable.Fly();
+        }
+    }
+}
