5c8caf69500645271ee079a0a3f5610cb7d13e57
NaHyKim <koloke1712@gmail.com>
2019-07-09 23:04:52 +0900

    Redirection

---
+++ Chapter14 Lambda/Lambda.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace CSharpStudy
+{
+    class Lambda
+    {
+
+    }
+
+    class MainApp
+    {
+        delegate int Calculate(int a, int b);
+        delegate string Concatenate(string[] args);
+
+        static void Main(string[] args)
+        {
+            Calculate calc = (a, b) => a + b;
+            Concatenate concat = (arr) =>
+            {
+                string result = "";
+                foreach (string str in args)
+                    result += str;
+                return result;
+            };
+            Func<int> func1 = () => 10;
+            Func<int, int> func2 = (x) => x;
+            Func<int, int, int> func3 = (x, y) => x * y;
+
+            Action act1 = () => Console.WriteLine("action1");
+            Action<int> act2 = (x) => Console.WriteLine(x * x);
+            Action<double, double> act3 = (x, y) => Console.WriteLine("div: {0}", x / y);
+
+            Console.WriteLine(calc(3, 4));
+            Console.WriteLine(concat(args));
+            Console.WriteLine(func1());
+            Console.WriteLine(func2(123));
+            Console.WriteLine(func3(23, 56));
+            act1();
+            act2(23);
+            act3(23.11, 56.3234);
+
+            // Using expression tree
+            Expression const1 = Expression.Constant(1); // 1
+            Expression const2 = Expression.Constant(2); // 2
+            Expression leftExp = Expression.Multiply(const1, const2); // 1*2
+
+            Expression param1 = Expression.Parameter(typeof(int)); // x
+            Expression param2 = Expression.Parameter(typeof(int)); // y
+            Expression rightExp = Expression.Subtract(param1, param2); // x-y
+
+            Expression exp = Expression.Add(leftExp, rightExp); // (1*2)+(x-y)
+            Expression<Func<int, int, int>> expression =
+                Expression<Func<int, int, int>>.Lambda<Func<int, int, int>>
+                (exp, new ParameterExpression[] { (ParameterExpression)param1, (ParameterExpression)param2 });
+
+            Func<int, int, int> func = expression.Compile();
+            Console.WriteLine($"1*2+({7}-{8}) = {func(7, 8)}");
+
+            // Using lambda
+            Expression<Func<int, int, int>> lambdaExp = (a, b) => 1 * 2 + (a - b);
+            Func<int, int, int> lambdaFunc = lambdaExp.Compile();
+            Console.WriteLine("Same result: {0}", lambdaFunc(7, 8));
+
+        }
+    }
+}
---
+++ Chapter16 Reflection, Attribute/Reflection.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace CSharpStudy
+{
+    class Reflection
+    {
+
+        // GetType/MainApp.cs
+
+        static void PrintInterfaces(Type type)
+        {
+            Console.WriteLine("----- Interfaces -----");
+
+            Type[] interfaces = type.GetInterfaces();
+            foreach (Type i in interfaces)
+                Console.WriteLine("Name: {0}", i.Name);
+
+            Console.WriteLine();
+        }
+
+        static void PrintFields(Type type)
+        {
+            Console.WriteLine("----- Fields -----");
+
+            FieldInfo[] fields = type.GetFields(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static | BindingFlags.Instance);
+            foreach (FieldInfo field in fields)
+            {
+                string accessLevel = "protected";
+                if (field.IsPublic) accessLevel = "public";
+                else if (field.IsPrivate) accessLevel = "private";
+
+                Console.WriteLine("Access: {0}, Type: {1}, Name: {2}", accessLevel, field.FieldType, field.Name);
+            }
+
+            Console.WriteLine();
+        }
+
+        static void PrintMethods(Type type)
+        {
+            Console.WriteLine("----- Methods -----");
+
+            MethodInfo[] methods = type.GetMethods();
+            foreach (MethodInfo method in methods)
+            {
+                Console.Write("Type: {0}, Name: {1}, Parameter: ", method.ReturnType.Name, method.Name);
+
+                ParameterInfo[] args = method.GetParameters();
+                for(var i=0; i<args.Length; i++)
+                {
+                    Console.Write("{0}", args[i].ParameterType.Name);
+                    if (i < args.Length - 1)
+                        Console.Write(", ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        static void PrintProperties(Type type)
+        {
+            Console.WriteLine("----- Properties -----");
+
+            PropertyInfo[] properties = type.GetProperties();
+            foreach (PropertyInfo property in properties)
+            {
+                Console.WriteLine("Type: {0}, Name: {1}", property.PropertyType.Name, property.Name);
+            }
+
+            Console.WriteLine();
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            /* GetType
+             * 
+            int a = 0;
+            Type type = a.GetType();
+
+            PrintInterfaces(type);
+            PrintFields(type);
+            PrintProperties(type);
+            PrintMethods(type);
+            */
+
+            /* Dynamic Instance
+             * 
+            Type type = Type.GetType("CSharpStudy.Profile");
+            MethodInfo methodInfo = type.GetMethod("Print");
+            PropertyInfo nameProperty = type.GetProperty("Name");
+            PropertyInfo phoneProperty = type.GetProperty("Phone");
+
+            object profile = Activator.CreateInstance(type, "이름", "123-4535");
+            methodInfo.Invoke(profile, null);
+
+            profile = Activator.CreateInstance(type);
+            nameProperty.SetValue(profile, "새이름", null);
+            phoneProperty.SetValue(profile, "99-9993", null);
+
+            Console.WriteLine("{0}, {1}", nameProperty.GetValue(profile, null), phoneProperty.GetValue(profile, null));
+            */
+
+            // Emit
+            AssemblyBuilder newAssembly = AppDomain.CurrentDomain.DefineDynamicAssembly(new AssemblyName("CalculatorAssembly"), AssemblyBuilderAccess.Run);
+
+            ModuleBuilder newModule = newAssembly.DefineDynamicModule("Calculator");
+            TypeBuilder newType = newModule.DefineType("Sum1To100");
+
+            MethodBuilder newMethod = newType.DefineMethod("Calculate", MethodAttributes.Public, typeof(int), new Type[0]);
+
+            ILGenerator generator = newMethod.GetILGenerator();
+
+            generator.Emit(OpCodes.Ldc_I4, 1);
+            for(int i=2; i<=100; i++)
+            {
+                generator.Emit(OpCodes.Ldc_I4, i);
+                generator.Emit(OpCodes.Add);
+            }
+
+            generator.Emit(OpCodes.Ret);
+            newType.CreateType();
+
+            object sum1To100 = Activator.CreateInstance(newType);
+            MethodInfo Calculate = sum1To100.GetType().GetMethod("Calculate");
+            Console.WriteLine(Calculate.Invoke(sum1To100, null));
+
+        }
+    }
+
+    // DynamicInstance/MainApp.cs
+
+    class Profile
+    {
+        private string name;
+        private string phone;
+
+        public Profile()
+        {
+            name = "";
+            phone = "";
+        }
+        public Profile(string name, string phone)
+        {
+            this.name = name;
+            this.phone = phone;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"{name}, {phone}");
+        }
+        
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Phone
+        {
+            get { return phone; }
+            set { phone = value; } 
+        }
+    }
+}
