fd397587c4d55eb251bb939b844b106d869f0b82
NaHyKim <koloke1712@gmail.com>
2019-07-15 11:18:28 +0900

    Add chapter15

---
+++ Attribute.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace CSharpStudy
+{
+    /*
+    class MyClass
+    {
+        [Obsolete("폐기된 method입니다. NewMethod를 사용하세요.")]
+        public void OldMethod()
+        {
+            Console.WriteLine("I'm old");
+        }
+
+        public void NewMethod()
+        {
+            Console.WriteLine("New");
+        }
+
+    }
+
+    public static class Trace
+    {
+        public static void WriteLine(string message, [CallerFilePath] string file = "", [CallerLineNumber] int line = 0, [CallerMemberName] string member = "")
+        {
+            Console.WriteLine($"(Line: {line}) {member} : {message}");
+        }
+    }
+    */
+
+    [System.AttributeUsage(System.AttributeTargets.Class, AllowMultiple = true)]
+    class History : System.Attribute
+    {
+        private string programmer;
+        public double version;
+        public string changes;
+
+        public History(string programmer)
+        {
+            this.programmer = programmer;
+            version = 1.0;
+            changes = "First release";
+        }
+
+        public string GetProgrammer()
+        {
+            return programmer;
+        }
+    }
+
+    [History("Sean", version = 0.1, changes = "Created class stub")]
+    [History("Bean", version = 0.2, changes = "Added Func()")]
+    class NewClass
+    {
+        public void Func()
+        {
+            Console.WriteLine("Func()");
+        }
+    }
+
+
+    class Attribute
+    {
+        static void Main(string[] args)
+        {
+            /*
+            MyClass obj = new MyClass();
+
+            obj.OldMethod();
+            obj.NewMethod();
+
+            Trace.WriteLine("멧시지");
+            */
+            Type type = typeof(NewClass);
+            Attribute[] attributes = Attribute.GetCustomAttributes(type);
+
+            Console.WriteLine("MyClass change history");
+
+            foreach(Attribute a in attributes)
+            {
+                History h = a as History;
+                if(h != null)
+                {
+                    Console.WriteLine($"Ver: {h.version}, Programmer: {h.GetProgrammer()}, Changes: {h.changes}");
+                }
+            }
+        }
+    }
+}
---
+++ Chapter15 LINQ/LINQ.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStudy
+{
+    class LINQ
+    {
+        class Profile
+        {
+            public string Name { get; set; }
+            public int Height { get; set; }
+        }
+
+        class Product
+        {
+            public string Title { get; set; }
+            public string Star { get; set; }
+        }
+
+
+        static void Main(string[] args)
+        {
+            int[] numbers = { 9, 2, 6, 4, 5, 3, 67, 9, 4, 3, 7 };
+            var result = from number in numbers
+                         where number % 2 == 0
+                         orderby number descending
+                         select number;
+
+            foreach (int number in result)
+                Console.WriteLine(number);
+
+            Profile[] arrProfile =
+            {
+                new Profile(){Name = "smklf", Height=233},
+                new Profile(){Name = "ㄴㅇㅇㄴ", Height=2},
+                new Profile(){Name = "vsvd", Height=12},
+                new Profile(){Name = "sdvsvd", Height=444},
+            };
+            Product[] arrProduct =
+            {
+                new Product(){Title = "ASDASDASD", Star = "smklf"},
+                new Product(){Title = "324324234", Star = "vsvd"},
+                new Product(){Title = "890890890890", Star = "pweorpiop"},
+                new Product(){Title = "@@@@@@", Star = "dankedanke"},
+                new Product(){Title = "#############", Star = "smklf"},
+
+            };
+            var listProfile = from profile in arrProfile
+                              join product in arrProduct on profile.Name equals product.Star
+                              select new
+                              {
+                                  Name = profile.Name,
+                                  Work = product.Title,
+                                  Height = profile.Height
+                              };
+
+            foreach(var profile in listProfile)
+            {
+                Console.WriteLine($"Name: {profile.Name}, WORK: {profile.Work}, Height: {profile.Height}");
+            }
+
+            listProfile = from profile in arrProfile
+                          join product in arrProduct on profile.Name equals product.Star into ps
+                          from product in ps.DefaultIfEmpty(new Product() { Title = "없음" })
+                          select new
+                          {
+                              Name = profile.Name,
+                              Work = product.Title,
+                              Height = profile.Height
+                          };
+
+            foreach (var profile in listProfile)
+            {
+                Console.WriteLine($"Name: {profile.Name}, WORK: {profile.Work}, Height: {profile.Height}");
+            }
+
+            var heightStat = from profile in arrProfile
+                             group profile by profile.Height < 300 into g
+                             select new
+                             {
+                                 Group = g.Key,
+                                 Count = g.Count(),
+                                 Max = g.Max(profile => profile.Height),
+                                 Min = g.Min(profile => profile.Height),
+                                 Average = g.Average(profile => profile.Height)
+                             };
+
+            foreach(var stat in heightStat)
+            {
+                Console.WriteLine($"{stat.Group} - Count: {stat.Count}, Max: {stat.Max}, Min: {stat.Min}, Average: {stat.Average}");
+            }
+
+        }
+    }
+}
