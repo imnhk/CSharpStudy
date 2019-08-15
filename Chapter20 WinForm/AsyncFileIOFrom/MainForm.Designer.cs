70d7b53639650ffde1f09e1faef4c59f38051e69
NaHyKim <koloke1712@gmail.com>
2019-08-15 12:27:11 +0900

    Finish chapter20

---
+++ Chapter20 WinForm/AsyncFileIOFrom/MainForm.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace AsyncFileIOFrom
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.txtTarget = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnFindSource = new System.Windows.Forms.Button();
+            this.btnFindTarget = new System.Windows.Forms.Button();
+            this.btnAsyncCopy = new System.Windows.Forms.Button();
+            this.btnSyncCopy = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.pbCopy = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Source :";
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(71, 41);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(248, 21);
+            this.txtSource.TabIndex = 1;
+            // 
+            // txtTarget
+            // 
+            this.txtTarget.Location = new System.Drawing.Point(71, 68);
+            this.txtTarget.Name = "txtTarget";
+            this.txtTarget.Size = new System.Drawing.Size(248, 21);
+            this.txtTarget.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Target :";
+            // 
+            // btnFindSource
+            // 
+            this.btnFindSource.Location = new System.Drawing.Point(325, 40);
+            this.btnFindSource.Name = "btnFindSource";
+            this.btnFindSource.Size = new System.Drawing.Size(41, 23);
+            this.btnFindSource.TabIndex = 4;
+            this.btnFindSource.Text = "...";
+            this.btnFindSource.UseVisualStyleBackColor = true;
+            this.btnFindSource.Click += new System.EventHandler(this.BtnFindSource_Click);
+            // 
+            // btnFindTarget
+            // 
+            this.btnFindTarget.Location = new System.Drawing.Point(325, 67);
+            this.btnFindTarget.Name = "btnFindTarget";
+            this.btnFindTarget.Size = new System.Drawing.Size(41, 23);
+            this.btnFindTarget.TabIndex = 5;
+            this.btnFindTarget.Text = "...";
+            this.btnFindTarget.UseVisualStyleBackColor = true;
+            this.btnFindTarget.Click += new System.EventHandler(this.BtnFindTarget_Click);
+            // 
+            // btnAsyncCopy
+            // 
+            this.btnAsyncCopy.Location = new System.Drawing.Point(12, 123);
+            this.btnAsyncCopy.Name = "btnAsyncCopy";
+            this.btnAsyncCopy.Size = new System.Drawing.Size(115, 23);
+            this.btnAsyncCopy.TabIndex = 6;
+            this.btnAsyncCopy.Text = "Async Copy";
+            this.btnAsyncCopy.UseVisualStyleBackColor = true;
+            this.btnAsyncCopy.Click += new System.EventHandler(this.BtnAsyncCopy_Click);
+            // 
+            // btnSyncCopy
+            // 
+            this.btnSyncCopy.Location = new System.Drawing.Point(133, 123);
+            this.btnSyncCopy.Name = "btnSyncCopy";
+            this.btnSyncCopy.Size = new System.Drawing.Size(130, 23);
+            this.btnSyncCopy.TabIndex = 7;
+            this.btnSyncCopy.Text = "Sync Copy";
+            this.btnSyncCopy.UseVisualStyleBackColor = true;
+            this.btnSyncCopy.Click += new System.EventHandler(this.BtnSyncCopy_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(269, 123);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(97, 23);
+            this.btnCancel.TabIndex = 8;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
+            // 
+            // pbCopy
+            // 
+            this.pbCopy.Location = new System.Drawing.Point(14, 153);
+            this.pbCopy.Name = "pbCopy";
+            this.pbCopy.Size = new System.Drawing.Size(352, 29);
+            this.pbCopy.TabIndex = 9;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 194);
+            this.Controls.Add(this.pbCopy);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSyncCopy);
+            this.Controls.Add(this.btnAsyncCopy);
+            this.Controls.Add(this.btnFindTarget);
+            this.Controls.Add(this.btnFindSource);
+            this.Controls.Add(this.txtTarget);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "Async File Copy";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.TextBox txtTarget;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnFindSource;
+        private System.Windows.Forms.Button btnFindTarget;
+        private System.Windows.Forms.Button btnAsyncCopy;
+        private System.Windows.Forms.Button btnSyncCopy;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.ProgressBar pbCopy;
+    }
+}
+
---
+++ Chapter20 WinForm/AsyncFileIOFrom/MainForm.cs
@@ -0,0 +1,98 @@
+﻿
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AsyncFileIOFrom
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private async Task<long> CopyAsync(string FromPath, string ToPath)
+        {
+            btnSyncCopy.Enabled = false;
+            long totalCopied = 0;
+
+            using (FileStream fromStream = new FileStream(FromPath, FileMode.Open))
+            {
+                using (FileStream toStream = new FileStream(ToPath, FileMode.Create))
+                {
+                    byte[] buffer = new byte[1024 * 1024];
+                    int nRead = 0;
+                    while((nRead = await fromStream.ReadAsync(buffer, 0 ,buffer.Length)) != 0)
+                    {
+                        await toStream.WriteAsync(buffer, 0, nRead);
+                        totalCopied += nRead;
+
+                        pbCopy.Value = (int)(((double)totalCopied / (double)fromStream.Length) * pbCopy.Maximum);
+                    }
+                }
+            }
+
+            btnSyncCopy.Enabled = true;
+            return totalCopied;
+        }
+
+        private long CopySync(string FromPath, string ToPath)
+        {
+            btnAsyncCopy.Enabled = false;
+            long totalCopied = 0;
+
+            using (FileStream fromStream = new FileStream(FromPath, FileMode.Open))
+            {
+                using (FileStream toStream = new FileStream(ToPath, FileMode.Create))
+                {
+                    byte[] buffer = new byte[1024 * 1024];
+                    int nRead = 0;
+                    while((nRead = fromStream.Read(buffer, 0, buffer.Length)) != 0)
+                    {
+                        toStream.Write(buffer, 0, nRead);
+                        totalCopied += nRead;
+
+                        pbCopy.Value = (int)(((double)totalCopied / (double)fromStream.Length) * pbCopy.Maximum);
+                    }
+                }
+            }
+
+            btnAsyncCopy.Enabled = true;
+            return totalCopied;
+        }
+
+        private void BtnFindSource_Click(object sender, System.EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if(dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtSource.Text = dlg.FileName;
+            }
+        }
+
+        private void BtnFindTarget_Click(object sender, System.EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtTarget.Text = dlg.FileName;
+            }
+        }
+
+        private async void BtnAsyncCopy_Click(object sender, System.EventArgs e)
+        {
+            long totalCopied = await CopyAsync(txtSource.Text, txtTarget.Text);
+        }
+
+        private void BtnSyncCopy_Click(object sender, System.EventArgs e)
+        {
+            long totalCopied = CopySync(txtSource.Text, txtTarget.Text);
+        }
+
+        private void BtnCancel_Click(object sender, System.EventArgs e)
+        {
+            MessageBox.Show("UI Reaction Test");
+        }
+    }
+}
