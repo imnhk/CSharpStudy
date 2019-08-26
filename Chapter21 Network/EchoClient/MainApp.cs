eda271672e29cc4295192660ec30ea5810f16d74
NaHyKim <koloke1712@gmail.com>
2019-08-26 17:54:32 +0900

    Add ch21
    Network

---
+++ Chapter21 Network/EchoClient/MainApp.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace EchoClient
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length < 4)
+            {
+                Console.WriteLine($"Usage: {Process.GetCurrentProcess().ProcessName} <Bind IP> <Bind Port> <Server IP> <Message>");
+                return;
+            }
+
+            string bindIp = args[0];
+            int bindPort = Convert.ToInt32(args[1]);
+            string serverIp = args[2];
+            const int serverPort = 5425;
+            string message = args[3];
+
+            try
+            {
+                IPEndPoint clientAddress = new IPEndPoint(IPAddress.Parse(bindIp), bindPort);
+                IPEndPoint serverAddress = new IPEndPoint(IPAddress.Parse(serverIp), serverPort);
+
+                Console.WriteLine($"Client: {clientAddress.ToString()}, Server: {serverAddress.ToString()}");
+
+                TcpClient client = new TcpClient(clientAddress);
+                client.Connect(serverAddress);
+
+                byte[] data = System.Text.Encoding.Default.GetBytes(message);
+
+                NetworkStream stream = client.GetStream();
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine($"Send: {message}");
+
+                data = new byte[256];
+
+                string responseData = "";
+
+                int bytes = stream.Read(data, 0, data.Length);
+                responseData = Encoding.Default.GetString(data, 0, bytes);
+                Console.WriteLine($"Receive: {responseData}");
+
+                stream.Close();
+                client.Close();
+            }
+            catch(SocketException e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.WriteLine("Closing client...");
+        }
+    }
+}
---
+++ Chapter21 Network/EchoServer/MainApp.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace EchoServer
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length < 1)
+            {
+                Console.WriteLine("Usage: {0} <Bind IP>", Process.GetCurrentProcess().ProcessName);
+                return;
+            }
+
+            string bindIP = args[0];
+            const int bindPort = 5425;
+            TcpListener server = null;
+            try
+            {
+                IPEndPoint localAddress = new IPEndPoint(IPAddress.Parse(bindIP), bindPort);
+                server = new TcpListener(localAddress);
+                server.Start();
+
+                Console.WriteLine("Echo server start...");
+
+                while (true)
+                {
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine($"Access client: {client.Client.RemoteEndPoint.ToString()}");
+
+                    NetworkStream stream = client.GetStream();
+
+                    int length;
+                    string data = null;
+                    byte[] bytes = new byte[256];
+
+                    while ((length = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        data = Encoding.Default.GetString(bytes, 0, length);
+                        Console.WriteLine(String.Format($"Received: {data}"));
+
+                        byte[] msg = Encoding.Default.GetBytes(data);
+                        stream.Write(msg, 0, msg.Length);
+                        Console.WriteLine(String.Format($"Send: {data}"));
+                    }
+
+                    stream.Close();
+                    client.Close();
+                }
+
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                server.Stop();
+            }
+
+            Console.WriteLine("Closing server...");
+        }
+    }
+}
